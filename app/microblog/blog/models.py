a685d78b5a862f297e781cd46bbe7a727c5a4352
devsia93 <sia93r@gmail.com>
2020-10-17 23:38:20 +0300

    added navbar; added display and switch pages: posts, tags; added card to post; implemented tag navigation.
    added migrations from class Post, Tag; added templates: base_blog.html, card_template.html, index.html, post_details.html, tag_detail.html, tags_list.html, base.html

--- app/microblog/blog/models.py
+++ app/microblog/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=75, unique=True)
+    body = models.TextField(blank=True, db_index=True) #blank - поле может быть пустым
+    date_pub = models.DateTimeField(auto_now_add=True) #автоматически заполняется при сохранении в бд
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug':self.slug})
+
+    def __str__(self):
+       return '{}'.format(self.title)
+
+    def check_available_tag(self):
+        if len(self.tags.all()) > 0:
+            return True
+        else :
+            return False
+
+class Tag(models.Model):
+    title = models.CharField(max_length=15)
+    slug = models.SlugField(max_length=25, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug':self.slug})
